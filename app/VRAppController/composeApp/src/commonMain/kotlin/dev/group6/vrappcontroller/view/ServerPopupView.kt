d9a4fddd646941580199ce3cc17b5454fd18be80
Karla Schramm <kschramm2@hof-university.de>
2025-11-15 18:25:24 +0100

    Font change for nonce

--- app/VRAppController/composeApp/src/commonMain/kotlin/dev/group6/vrappcontroller/view/ServerPopupView.kt
+++ app/VRAppController/composeApp/src/commonMain/kotlin/dev/group6/vrappcontroller/view/ServerPopupView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -13,6 +14,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.Popup
@@ -48,6 +50,7 @@ fun ServerPopupView(
                     value = nonce,
                     readOnly = true,
                     onValueChange = {},
+                    textStyle = MaterialTheme.typography.bodyLarge.copy(fontFamily = FontFamily.Monospace),
                     label = { Text("Nonce") },
                 )
                 Button(
